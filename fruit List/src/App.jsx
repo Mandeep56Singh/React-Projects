2c23ceae5433e7f5514fbb2686bf5169a83238ec
mandeepSingh <mandeepraj2312@gmail.com>
2024-03-26 23:45:15 +0530

    refactor : make condition rendering jsx into separate function

--- fruit List/src/App.jsx
+++ fruit List/src/App.jsx
@@ -2,24 +2,23 @@ import { useState } from "react";
 
 function App() {
   const [fruits, setFruits] = useState(["apple", "mango", "banana"]);
-  const [fruitCount,setFruitCount] = useState(3);
+  const [fruitCount, setFruitCount] = useState(3);
   const totolFruits = 7;
-
 
   const handleAddFruit = () => {
     const val = document.getElementById("fruit1").value;
     document.getElementById("fruit1").value = "";
-    const checkFruit = fruits.map( (fruit) => fruit.lowercase);
-    if(!checkFruit.includes(val.lowercase)) {
-    setFruits((f) => [...f, val]);
-     setFruitCount((f) => f + 1);
+    const checkFruit = fruits.map((fruit) => fruit.lowercase);
+    if (!checkFruit.includes(val.lowercase)) {
+      setFruits((f) => [...f, val]);
+      setFruitCount((f) => f + 1);
     } else {
-      alert('fruit already exist')
+      alert("fruit already exist");
     }
   };
-   const handleEmptyBasket = () => {
-    return( fruits.length === 0 ? <h1>add something to basket</h1> : null)
-   }
+  const handleEmptyBasket = () => {
+    return fruits.length === 0 ? <h1>add something to basket</h1> : null;
+  };
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       handleAddFruit();
@@ -27,34 +26,41 @@ function App() {
   };
   const handleRemoveFruit = (fruitR) => {
     handleEmptyBasket();
-    setFruits(
-     fruits =>   fruits.filter( fruit => fruit !== fruitR )
-    );
+    setFruits((fruits) => fruits.filter((fruit) => fruit !== fruitR));
 
     setFruitCount((f) => f - 1);
-    
-   
-  }
+  };
   const handleClearAll = () => {
     setFruits([]);
     setFruitCount(0);
-  }
+  };
 
+  const BasketEmpty = () => {
+    return fruits.length === 0 ? (
+      <p className="text-2xl text-center mt-5">Add some fruits to basket</p>
+    ) : null;
+  };
+
+  const LeftFruits = () => {
+    return fruits.length !== 0 && totolFruits - fruitCount > 0 ? (
+      <p className="text-2xl text-center mt-5">
+        {totolFruits - fruitCount} more fruits can be placed
+      </p>
+    ) : null;
+  };
+  const BasketFull = () => {
+    return fruits.length === totolFruits ? <p className="text-2xl text-center mt-5"> Basket is full</p> : null;
+  }
   return (
     <div className="m-10">
       <h1 className="text-4xl text-center">Fruit Basket</h1>
 
-      {fruits.length === 0 && (
-        <p className="text-2xl text-center mt-5">Add some fruits to basket</p>
-      )}
-      {fruits.length !== 0 && totolFruits - fruitCount > 0 && (
-        <p className="text-2xl text-center mt-5">
-          {totolFruits - fruitCount} more fruits can be placed
-        </p>
-      )}
-      {fruits.length === totolFruits && (
+      <BasketEmpty />
+      <LeftFruits />
+      {/* {fruits.length === totolFruits && (
         <p className="text-2xl text-center mt-5"> Basket is full</p>
-      )}
+      )} */}
+      <BasketFull />
 
       <ul className="mt-10">
         {fruits.map((fruit, index) => (
