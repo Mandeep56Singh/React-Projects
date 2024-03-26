4d56ba76d492ba69d2bc7dd3fb76dc90a9754eb4
mandeepSingh <mandeepraj2312@gmail.com>
2024-03-26 23:07:46 +0530

    feat(fruit List):handle duplicate fruits by showing alert

--- fruit List/src/App.jsx
+++ fruit List/src/App.jsx
@@ -9,8 +9,12 @@ function App() {
   const handleAddFruit = () => {
     const val = document.getElementById("fruit1").value;
     document.getElementById("fruit1").value = "";
+    if(!fruits.includes(val)) {
     setFruits((f) => [...f, val]);
      setFruitCount((f) => f + 1);
+    } else {
+      alert('fruit already exist')
+    }
   };
    const handleEmptyBasket = () => {
     return( fruits.length === 0 ? <h1>add something to basket</h1> : null)
