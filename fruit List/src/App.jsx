449d19862f3746b553a26d79013c616a92ccc644
mandeepSingh <mandeepraj2312@gmail.com>
2024-03-27 15:13:49 +0530

    feat(Fruit List):add feature to sort the elements alphabetically

--- fruit List/src/App.jsx
+++ fruit List/src/App.jsx
@@ -4,13 +4,24 @@ function App() {
   const [fruits, setFruits] = useState(["apple", "mango", "banana"]);
   const [fruitCount, setFruitCount] = useState(3);
   const totolFruits = 7;
+  const [filters, setFilters] = useState({
+    sortAlphabetically:false
+  });
 
  const handleAddFruit = () => {
    const val = document.getElementById("fruit1").value;
    document.getElementById("fruit1").value = "";
    const checkFruit = fruits.map((fruit) => fruit.toLowerCase());
+    
    if (!checkFruit.includes(val.toLowerCase()) && fruits.length < totolFruits) {
-     setFruits((f) => [...f, val]);
+setFruits((f) => {
+      const newFruits = [...f, val];
+      if (filters.sortAlphabetically) {
+        newFruits.sort((a, b) => a.localeCompare(b));
+      }
+      // handle other filters
+      return newFruits;
+    });
      setFruitCount((f) => f + 1);
    } else if  (checkFruit.includes(val.toLowerCase())) {
      alert("fruit already exist");
@@ -18,6 +29,7 @@ function App() {
    else if (!fruits.length < totolFruits) {
      alert("Can't add more fruits ");
    }
+
  };
 
   const handleEmptyBasket = () => {
@@ -38,7 +50,19 @@ function App() {
     setFruits([]);
     setFruitCount(0);
   };
+const handleFilter = (e) => {
+   const filter = e.target.value;
+  if (filter === "Sort Alphabetically") {
+    const sortedArray = [...fruits].sort((a, b) => a.localeCompare(b));
+    setFruits(sortedArray);
+    setFilters((f) => ({ ...f, sortAlphabetically: true }));
+  } else {
+    setFilters((f) => ({ ...f, sortAlphabetically: false }));
+  }
+};
 
+
+//  JSX Elements 
   const BasketEmpty = () => {
     return fruits.length === 0 ? (
       <p className="text-2xl text-center mt-5">Add some fruits to basket</p>
@@ -98,6 +122,17 @@ function App() {
           ADD
         </button>
       </div>
+
+      <select
+        name="filters"
+        id="filters"
+        className="bg-gray-300 py-3 px-4 rounded-xl text-xl text-gray-900 mt-4 focus:outline-gray-400"
+        onChange={handleFilter}
+      >
+        <option value="">filters</option>
+        <option value="Sort Alphabetically">Sort Alphabetically</option>
+      </select>
+
       <button
         className="bg-gray-300 py-3 px-4 rounded-xl ml-2 text-xl text-gray-900 mt-4"
         onClick={handleClearAll}
